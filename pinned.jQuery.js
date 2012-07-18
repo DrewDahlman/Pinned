985e67e2401a61c5ae9738b806f9e4f534931c73
DrewDahlman <info@drewdahlman>
2012-07-18 00:54:23 -0600

    Initial commit

---
+++ pinned.jQuery.js
@@ -0,0 +1,89 @@
+/*
+	Plugin: Pinned
+	Author: Drew Dahlman ( www.drewdahlman.com )
+	Version: 1.0
+*/
+
+(function($) {
+
+	$.fn.pinned = function(options,pinning,unpinned) {
+		var defaults = {
+			bounds: '0px',
+			scrolling: '0px',
+			mobile: false
+		}
+
+		if(pinning && unpinned){
+			var callback = {
+				pinning: pinning,
+				unpinned: unpinned
+			}
+		}
+		
+		return this.each(function() {
+
+			var settings = $.extend(defaults,options);
+			var callbacks = $.extend(callback,pinning,unpinned);
+
+			var $this = $(this);
+			var orig = $this.css('top');
+			var pinnedTimeout = 0;
+
+			function init(){
+				if(isMobile() && setings.mobile == false || !isMobile() ){
+					console.log('desktop only');
+					windowScroll();
+				}
+				else{
+					console.log('mobile only');
+					mobileScroll();
+				}
+			}
+
+			windowScroll = function(){
+				$(window).bind('scroll',function(){
+					if ($(window).scrollTop() > settings.bounds && $this.css('position') != 'fixed' ){ 
+						$this.css({'position': 'fixed', 'top': settings.scrolling});
+						if(callbacks.pinning != null){
+							callbacks.pinning();
+						}
+					} 
+					if ($(window).scrollTop() < settings.bounds && $this.css('position') != 'absolute'){ 
+						$this.css({'position': 'absolute', 'top': orig}); 
+						if(callbacks.unpinned != null){
+							callbacks.unpinned();
+						}
+					}
+				});
+			}
+
+			// TODO - Add better support for mobile devices on scroll
+			mobileScroll = function(){
+				$(window).bind('touchmove',function(){
+					if ($(window).scrollTop() > settings.bounds && $this.css('position') != 'fixed' ){ 
+						$this.css({'position': 'fixed', 'top': settings.scrolling});
+						
+					} 
+					if ($(window).scrollTop() < settings.bounds && $this.css('position') != 'absolute'){ 
+						$this.css({'position': 'absolute', 'top': orig}); 
+						
+					}
+				});
+			}
+
+			isMobile = function(){
+				if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPad/i))) {
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+			init();
+    });
+	};
+	$.fn.unpin = function(){
+		$(window).unbind('scroll');
+	}
+
+})(jQuery);
---
+++ readme.md
@@ -0,0 +1,16 @@
+##Pinned
+A jQuery plugin for creating "pinned" or sticky elements on a page.
+
+##Features
+Quickly create pinned elements on page, also support mobile devices such as mobile safari.
+
+##Use
+<pre>
+	$("#element").pinned({
+		bounds: '100px',
+		scrolling: '110px',
+		mobile: false
+	});
+</pre>
+
+To use Pinned call the plugin by feeding your element selector, either ID or class.
